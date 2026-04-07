c09de40bd867a2990f22396fa38ce0da5f4f8a94
Cecilia Farias <cecyaf25@gmail.com>
2026-04-06 23:54:16 -0300

    Agrego class Monitor

--- JavaScript/Clase1/Tarea1/MundoPC.js
+++ JavaScript/Clase1/Tarea1/MundoPC.js
@@ -61,3 +61,43 @@ let teclado1 = new Teclado('Bluetooth', 'Logitec');
 let teclado2 = new Teclado('USB', 'HP');
 console.log(teclado1);
 console.log(teclado2);
+
+class Monitor {
+    static contadorMonitores = 0;
+
+    constructor(marca, tamaño){
+        this._idMonitor = ++Monitor.contadorMonitores;
+        this._marca = marca;
+        this._tamaño = tamaño;
+    }
+
+    get idMonitor(){
+        return this._idMonitor;
+    }
+
+    get marca(){
+        return this._marca;
+    }
+
+    set marca(marca){
+        this._marca = marca;
+    }
+
+    get tamaño(){
+        return this._tamaño;
+    }
+
+    set tamaño(tamaño){
+        this._tamaño = tamaño;
+    }
+
+    toString(){
+        return Monitor: [idMonitor: ${this._idMonitor}, Marca: ${this._marca}, Tamaño: ${this._tamaño}];
+    }
+}
+
+let monitor1 = new Monitor('Samsung', '24 pulgadas');
+let monitor2 = new Monitor('LG', '27 pulgadas');
+
+console.log(monitor1.toString());
+console.log(monitor2.toString());
