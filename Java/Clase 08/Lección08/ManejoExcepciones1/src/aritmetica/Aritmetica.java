e78add9594b7d5b15726a2aa3b2030503bd9b6ba
Lucas Nicolas Aguilar <aguilar666lucas@gmail.com>
2026-06-01 02:58:54 -0300

    Agrego la clase 08 de JAVA

---
+++ Java/Clase 08/Lección08/ManejoExcepciones1/src/aritmetica/Aritmetica.java
@@ -0,0 +1,16 @@
+package aritmetica;
+
+import excepciones.OperacionExcepcion; 
+
+public class Aritmetica {
+
+    public static int division(int numerador, int denominador) throws OperacionExcepcion {
+        
+        if (denominador == 0) {
+            
+            throw new OperacionExcepcion("División entre cero");
+        }
+        
+        return numerador / denominador;
+    }
+}
