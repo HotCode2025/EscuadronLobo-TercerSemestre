52eb9ba021f5e346247efdcbf7368957875402c4
Valentin Bertolone <valentinbertolone@gmail.com>
2026-06-30 18:03:25 -0300

    Delete Python/Clase07 directory

--- Python/Clase07/persona.py
+++
@@ -1,9 +0,0 @@
-class Persona:
-    def __init__(self, id_persona=None, nombre="", apellido="", email=""):
-        self.id_persona = id_persona
-        self.nombre = nombre
-        self.apellido = apellido
-        self.email = email
-
-    def __str__(self):
-        return f"{self.id_persona} - {self.nombre} {self.apellido} ({self.email})"
